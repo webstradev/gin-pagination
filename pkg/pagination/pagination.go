5af59657c8669e20d0855bd983a21bc9abb1464e
Erik <e.s.westra.95@gmail.com>
2025-03-07 17:28:34 +0100

    Make header setting part of pagination behavior and test accordingly.
    Signed-off-by: Erik <e.s.westra.95@gmail.com>

--- pkg/pagination/pagination.go
+++ pkg/pagination/pagination.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -95,11 +96,20 @@ func (p *paginator) validatePageSize(size int) error {
 
 func (p *paginator) setPageAndPageSize(page int, size int) {
 	p.c.Set(p.opts.PageText, page)
-	p.c.Header(p.opts.PageText, strconv.Itoa(page))
 	p.c.Set(p.opts.SizeText, size)
-	p.c.Header(p.opts.SizeText, strconv.Itoa(size))
+
+	p.c.Header(ConstructHeader(p.opts.PageText), strconv.Itoa(page))
+	p.c.Header(ConstructHeader(p.opts.SizeText), strconv.Itoa(size))
 }
 
 func (p *paginator) Next() {
 	p.c.Next()
+}
+
+// ConstructHeader creates a standardized header name with X- prefix and proper casing
+func ConstructHeader(text string) string {
+	if text == "" {
+		return ""
+	}
+	return fmt.Sprintf("X-%s%s", strings.ToUpper(text[:1]), text[1:])
 }
